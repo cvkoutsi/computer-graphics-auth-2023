930d07e829318b3385560b19e29e9d247ffb778a
cvkoutsi <ckoutsi26@gmail.com>
2023-04-25 19:21:00 +0300

    add h1 and flats function

---
+++ flats.py
@@ -0,0 +1,11 @@
+import numpy as np
+# The edges of the triangles. Size of L x 2 and contains the X and Y coordinates of each edge.
+verts2d = np.load("h1.npy", allow_pickle=True).tolist()['verts2d']
+# The colors of the edges. Size of L x 3 and contains the RGB values for each i-th edge.
+vcolors = np.load("h1.npy", allow_pickle=True).tolist()['vcolors']
+# Contains the faces of the triangles as a K x 3 array. Each of the K rows contains the indexes for the verts2d
+# triangle edges.
+faces = np.load("h1.npy", allow_pickle=True).tolist()['faces']
+# Contains the depth of each edge. Size of L x 1
+depth = np.load("h1.npy", allow_pickle=True).tolist()['depth']
+breakpoint()
